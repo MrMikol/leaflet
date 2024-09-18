d7a061cf9bb7f06004bd234f95da1b939c455b66
Michael Tan <michaelneiltan04@gmail.com>
2024-09-18 15:07:40 -0400

    adding markers

--- script.js
+++ script.js
@@ -1,54 +1,53 @@
-var map = L.map('map').fitWorld();//setView([10.244536, -61.455111], 13);
-var marker = L.marker([10.244536, -61.455111]).addTo(map);
-//var circle = L.circle([10.26, -61.45], {
-    //color: 'white',
-    //fillColor: '#ffff',
-    //fillOpacity: 0.3,
-    //radius: 3000
-//}).addTo(map);
-    
-
-var popup = L.popup()
-    .setLatLng([10.26, -61.45])
-    .setContent("I am a standalone popup.")
-    .openOn(map);
-
-map.locate({setView: true, maxZoom:16});
-
-//On Location Found
-//function onLocationFound(e) {
-    //var radius = e.accuracy;
-
-    //L.marker(e.latlng).addTo(map)
-        //.bindPopup("You are within " + radius + " meters from this point").openPopup();
-
-    //L.circle(e.latlng, radius).addTo(map);
-//}
-
-//map.on('locationfound', onLocationFound);
-
-//On location Error
-//function onLocationError(e) {
-    //alert(e.message);
-//}
-
-//map.on('locationerror', onLocationError);
-
-function onMapClick(e) {
-    popup
-        .setLatLng(e.latlng)
-        .setContent("" + e.latlng)
-        .openOn(map);
-}
-    
-map.on('click', onMapClick);
+var map = L.map('map').setView([10.244536, -61.455111], 13); //fitWorld()
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-marker.bindPopup("<b>Charity Baptist Church</b><br>94 S.S. Erin Road, Philipine, San Fernando").openPopup();
-//circle.bindPopup("I am a circle.");
-polygon.bindPopup("I am a polygon.");
+let tempMarker;
+let pinData = {}; //STORES PIN DATA
 
+//CLICK EVENT TO PLACE A PIN
+map.on('click', function (e) {
+    if (tempMarker) {
+        map.removeLayer(tempMarker);
+    }
+
+    //PLACES TEMPORARY MARKER ON THE CLICKED LOCATION
+    tempMarker = L.marker(e.latlng).addTo(map);
+
+    //SHOW FORM TO ENTER NAME AND DESCRIPTION
+    $('#overlay').css("display", "block"); // Show overlay
+    $('#pinModal').css("display", "block"); // Show modal
+    pinData = {
+        lat: e.latlng.latl,
+        lng: e.latlng.lng
+    };
+});
+
+$('#pinForm').on('submit', function(event) {
+    event.preventDefault();
+
+    const name = $('#pinName').val();
+    const description = $('#pinDescription').val();
+
+    if (tempMarker) {
+        //BIND POPUP WITH NAME AND DESCRIPTION
+        tempMarker.bindPopup(`<b>${name}</b><br>${description}`).openPopup();
+
+        //HIDE FORM
+        $('#overlay').css("display", "none");
+        $('#pinModal').css("display", "none");
+
+        //SAVE PIN INFORMATION
+        pinData.name = name;
+        pinData.description = description;
+        savePin(pinData);
+    }
+});
+
+function savePin(data) {
+    console.log('Pin saved:', data);
+    alert('Pin saved:', data);
+};
