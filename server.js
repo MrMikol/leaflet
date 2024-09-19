42805c6e9d3c4c784808b1bdf16b17aa99ec0ce5
Michael Tan <michaelneiltan04@gmail.com>
2024-09-19 14:29:47 -0400

    middleware and MongoDB connection

--- server.js
+++ server.js
@@ -3,3 +3,18 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+//MongoDB connection
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+  .then(() => console.log('Connect to MongoDB'))
+  .catch((err) => console.error('Failed to connect to MongoDB:', err));
